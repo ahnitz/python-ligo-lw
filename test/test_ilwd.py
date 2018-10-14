2e6ed1ca5dbdb62bc48b7da7d8a03533cd7af91b
Kipp Cannon <kipp.cannon@ligo.org>
2018-10-14 18:55:59 +0900

    more test/Makefile simplification

--- test/test_ilwd.py
+++ test/test_ilwd.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env python
+
 import doctest
 import sys
 from ligo.lw import _ilwd
 from ligo.lw import ilwd
 
-failures = doctest.testmod(_ilwd)[0]
-failures += doctest.testmod(ilwd)[0]
+if __name__ == "main":
+	failures = doctest.testmod(_ilwd)[0]
+	failures += doctest.testmod(ilwd)[0]
 
-sys.exit(bool(failures))
+	sys.exit(bool(failures))
